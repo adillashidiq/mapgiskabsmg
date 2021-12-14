10f12ffefddb73e007735174f7de63c279f81433
rafandi-huda <rafandi.maulana@gmail.com>
2021-12-14 13:30:40 +0700

    Menambahkan method index, marker,j polyline, rute, polygon

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,37 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'title' => 'View Map GIS | Kab. Semarang'
+        ];
+        return view('index', $data);
+    }
+    public function marker()
+    {
+        $data = [
+            'title' => 'Marker Map GIS | Kab. Semarang'
+        ];
+        return view('marker', $data);
+    }
+    public function polyline()
+    {
+        $data = [
+            'title' => 'Polyline Map GIS | Kab. Semarang'
+        ];
+        return view('polyline', $data);
+    }
+    public function rute()
+    {
+        $data = [
+            'title' => 'Rute Map GIS | Kab. Semarang'
+        ];
+        return view('rute', $data);
+    }
+    public function polygon()
+    {
+        $data = [
+            'title' => 'Polygon Map GIS | Kab. Semarang'
+        ];
+        return view('polygon', $data);
     }
 }

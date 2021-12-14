a80eea1d4214a6f8d6498c3a751651ec3ef6ab21
rafandi-huda <rafandi.maulana@gmail.com>
2021-12-14 13:33:25 +0700

    menambahkan file views rute.php

---
+++ app/Views/rute.php
@@ -0,0 +1,75 @@
+<?php
+echo $this->extend('layout/template');
+
+echo $this->section('content');
+echo $this->include('layout/navbar');
+echo $this->include('layout/sidenav');
+?>
+
+<div class="wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Rute</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div id="map" style="height: 500px;"></div>
+
+            <script>
+                var map = L.map('map').setView([-7.258366, 110.463861, -0.09], 12);
+
+                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    id: 'mapbox/streets-v11',
+                }).addTo(map);
+                L.Routing.control({
+                    waypoints: [
+                        L.latLng(-7.294378, 110.360185),
+                        L.latLng(-7.294258, 110.473294)
+                    ],
+                    routeWhileDragging: true
+                }).addTo(map);
+            </script>
+            <!-- right col -->
+    </div>
+    <!-- /.row (main row) -->
+</div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.1.0
+    </div>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<?= $this->endSection(); ?>
